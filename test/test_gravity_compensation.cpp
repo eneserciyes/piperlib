9990857ec084e1171cb4ca789735cad52bbd62e0
eneserciyes <m.eneserciyes@gmail.com>
2025-11-23 17:20:42 -0500

    add args to test_gravity_compenstation

--- test/test_gravity_compensation.cpp
+++ test/test_gravity_compensation.cpp
@@ -3,24 +3,62 @@
 #include "utils.h"
 #include <signal.h>
 #include <spdlog/spdlog.h>
+#include <iostream>
+#include <string>
 
 static bool running = true;
 
 void signal_handler(int signal) { running = false; }
 
+void print_usage(const char* prog_name) {
+    std::cout << "Usage: " << prog_name << " [options]\n"
+              << "Options:\n"
+              << "  -i, --interface <name>  CAN interface name (default: can_left)\n"
+              << "  -u, --urdf <path>       Path to URDF file (default: ../urdf/piper_no_gripper_description.urdf)\n"
+              << "  -g, --gripper           Enable gripper (default: false)\n"
+              << "  -h, --help              Show this help message\n";
+}
+
 int main(int argc, char *argv[]) {
   signal(SIGINT, signal_handler);
 
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " <urdf_file_path>" << std::endl;
-    return 1;
+  // Default configuration
+  std::string interface_name = "can_left";
+  std::string urdf_path = "../urdf/piper_no_gripper_description.urdf";
+  bool gripper_on = false;
+
+  // Parse arguments
+  for (int i = 1; i < argc; ++i) {
+    std::string arg = argv[i];
+    if (arg == "-h" || arg == "--help") {
+        print_usage(argv[0]);
+        return 0;
+    } else if (arg == "-i" || arg == "--interface") {
+        if (i + 1 < argc) {
+            interface_name = argv[++i];
+        } else {
+            std::cerr << "Error: --interface requires an argument\n";
+            return 1;
+        }
+    } else if (arg == "-u" || arg == "--urdf") {
+        if (i + 1 < argc) {
+            urdf_path = argv[++i];
+        } else {
+            std::cerr << "Error: --urdf requires an argument\n";
+            return 1;
+        }
+    } else if (arg == "-g" || arg == "--gripper") {
+        gripper_on = true;
+    } else {
+        std::cerr << "Unknown argument: " << arg << "\n";
+        print_usage(argv[0]);
+        return 1;
+    }
   }
 
-
-
-  std::string urdf_path = argv[1];
   InverseDynamicsSolver solver(urdf_path);
-  PiperInterface piper_interface("can_left");
+  PiperInterface piper_interface(interface_name, gripper_on);
+  
   if (piper_interface.get_arm_status() == ArmStatus::EMERGENCY_STOP) {
     spdlog::error("Arm status is emergency stop, first resume emergency stop!");
     exit(1);
@@ -28,7 +66,9 @@ int main(int argc, char *argv[]) {
 
   // reset arm
   reset_arm(piper_interface);
-  reset_gripper(piper_interface);
+  if (gripper_on) {
+    reset_gripper(piper_interface);
+  }
 
   RateLimiter rate_limiter(200);
 
@@ -52,6 +92,8 @@ int main(int argc, char *argv[]) {
   spdlog::info("Press Enter to disable arm...");
   std::cin.get();
   piper_interface.disable_arm();
-  piper_interface.disable_gripper();
+  if (gripper_on) {
+    piper_interface.disable_gripper();
+  }
   return 0;
 }
